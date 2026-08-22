c8220869dd67fc2a0ee4532d52d3a3af1c261cb0
luscinia <luscinia@utexas.edu>
2026-08-22 14:54:56 -0500

    Initial commit for La Fresita Sweets website

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// LANGUAGE CHANGER CODE - that I apparently won't use, fml
+let langData = {
+    "languages": {
+        "en": {
+            "strings": {
+                "hook-header": "Ice Creams, Sweets, flavored waters"
+            }
+        }, "es": {
+            "strings": {
+                "hook-header": "¡Helados, Dulces, Aguas Frescas y Más!"
+            }
+        }, "ja": {
+            "strings": {
+                "hook-header": "アイスクリームやキャンディーやフレッシュジュースなど！"
+            }
+        }
+    }
+}
