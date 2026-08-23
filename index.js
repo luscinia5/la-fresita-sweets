43f7a0fe5f2c05e2f6b34297e4e1421c4c3a0318
luscinia <luscinia@utexas.edu>
2026-08-23 13:20:04 -0500

    Translated the whole front page

--- index.js
+++ index.js
@@ -21,9 +21,21 @@ async function applyLanguage(lang) {
     document.querySelectorAll('[data-i18n]').forEach(el => {
         const key = el.dataset.i18n;
         const value = key.split('.').reduce((obj, k) => obj?.[k], dict);
-        if (value) el.textContent = value;
+        if (value) el.innerHTML = value; // used to be textContent
     });
 
     document.documentElement.lang = lang;
     localStorage.setItem('lang', lang);
 }
+
+/* 
+Note: innerHTML has a security risk where, if the string injected ever
+comes from user input (a comment form, a URL parameter, etc), this is exactly
+the mechanism behind XSS attacks. Malicious <script> tags could get
+injected and run.
+
+That is not a risk here because these strings are from JSON files that
+I write myself, but it's worth noting to not reflexively use innerHTML
+later if I add a comment section or a search box that echoes back what
+someone typed.
+*/
